928ed8d0c38cac699256d4349ed52e3915aac3e2
JeremyLapham <jeremylapham2004@gmail.com>
2023-02-14 15:35:56 -0800

    last commit before turn in still needs some work

--- scripts/app.js
+++ scripts/app.js
@@ -2,34 +2,38 @@ import { saveToLocalStorage, getLocalStoage, removeFromLocalStorage } from './lo
 
 const budgetNum = document.getElementById('budgetNum');
 const submitBtn = document.getElementById('submitBtn');
-const money = document.getElementById('money');
+const moneyTxt = document.getElementById('money');
 
 const expensePrice = document.getElementById('expensePrice');
 const expenseName = document.getElementById('expenseName');
 const submitExpenseBtn = document.getElementById('submitExpenseBtn');
 const whereExpense = document.getElementById('whereExpense');
 
-const placeHold = document.getElementById('placeHold');
-
 
 submitBtn.addEventListener('click', function () {
     if (budgetNum.value == '') {
         alert('Please Enter In Your Monthly Budget')
     } else {
-        money.textContent = 'Your budget is $' + budgetNum.value;
+        moneyTxt.textContent = 'Your budget is $' + budgetNum.value;
         submitExpenseBtn.classList.remove('hide');
     }
 });
 
+
 submitExpenseBtn.addEventListener('click', function () {
-    let saveMoney = {
-        budget: budgetNum.value,
-        expensename: expenseName.value.charAt(0).toUpperCase() + expenseName.value.slice(1),
-        expenseprice: expensePrice.value,
-        totalbudget: 0
-    };
-    saveMoney.totalbudget = (saveMoney.budget - saveMoney.expenseprice);
-    saveToLocalStorage(saveMoney);
+    let letters = /^[A-Za-z-]+$/;
+    if (expenseName.value == '' || expensePrice.value == ''|| !expenseName.value.match(letters)) {
+        alert('Please input something to the input fields or make sure you put letters into the name')
+    } else {
+
+        let saveMoney = {
+            budget: budgetNum.value,
+            expensename: expenseName.value.charAt(0).toUpperCase() + expenseName.value.slice(1),
+            expenseprice: expensePrice.value,
+            totalbudget: 0
+        };
+        saveToLocalStorage(saveMoney);
+    }
 });
 
 function CreateExpense() {
@@ -38,8 +42,8 @@ function CreateExpense() {
     let moneys = getLocalStoage();
 
     moneys.forEach(money => {
+        moneyTxt.textContent = 'Your budget is $' + money.budget;
         money.totalbudget = (money.budget - money.expenseprice);
-        placeHold.textContent = money.totalbudget;
 
         let row = document.createElement('row');
 
@@ -52,13 +56,17 @@ function CreateExpense() {
         deleteBtn.className = 'btn btn-danger';
         deleteBtn.textContent = 'Delete';
         deleteBtn.type = 'button';
+        deleteBtn.classList.add('expenseDisplay');
         deleteBtn.addEventListener('click', function () {
             deleteBtn.remove();
             h4.remove();
+            //i know this is the issue with why it adds all of them back but i cant figure out how to fix it 
             money.totalbudget += Number(money.expenseprice);
-            placeHold.textContent = money.totalbudget;
+
+            placeHold.textContent = `Remaining budget: $${money.totalbudget}`;
             removeFromLocalStorage(money);
         });
+        placeHold.textContent = `Remaining budget: $${money.totalbudget}`;
 
         row.appendChild(h4);
         row.appendChild(deleteBtn);
@@ -66,6 +74,5 @@ function CreateExpense() {
     })
 }
 CreateExpense()
-
 
 export { CreateExpense }
--- scripts/localstorage.js
+++ scripts/localstorage.js
@@ -1,29 +1,42 @@
 import { CreateExpense } from "./app.js";
+
+const placeHold = document.getElementById('placeHold');
+
 function saveToLocalStorage(amount) {
     let sameExpense = false;
     let money = getLocalStoage();
 
-    for(let i = 0; i<money.length; i++){
-        if(money[i].budget==amount.budget&&
-            money[i].expensename==amount.expensename&&
-            money[i].expenseprice==amount.expenseprice&&
-            money[i].totalbudget==amount.totalbudget) {
-                sameExpense = true;
-            }
+    for (let i = 0; i < money.length; i++) {
+        if (money[i].budget == amount.budget &&
+            money[i].expensename == amount.expensename &&
+            money[i].expenseprice == amount.expenseprice) {
+            sameExpense = true;
+        }
     }
-    if(sameExpense) {
-        alert('Hey hey hey are you really doing the same espense twice in a month')
+    if (sameExpense) {
+        alert('Hey hey hey are you really doing the same expense twice in a month')
     } else {
         money.push(amount);
-        localStorage.setItem('Money', JSON.stringify(money));
-        CreateExpense()
+        let total = 0;
+        for (let i = 0; i < money.length; i++) {
+            total += Number(money[i].expenseprice);
+        }
+        amount.totalbudget = amount.budget - total;
+        if(amount.totalbudget < 0) {
+            alert('Woahhhh you need to budget better there my dude')
+        } else {
+            placeHold.textContent = `Remaining budget: $${Number(amount.totalbudget)}`;
+            
+            localStorage.setItem('Money', JSON.stringify(money));
+            CreateExpense()
+        }
     }
 }
 
 function getLocalStoage() {
     let localStorageData = localStorage.getItem('Money');
 
-    if(localStorageData == null){
+    if (localStorageData == null) {
         return [];
     }
 
@@ -34,16 +47,16 @@ function removeFromLocalStorage(amount) {
     let money = getLocalStoage();
     let moneyIndex = 0;
 
-    for(let i = 0; i<money.length; i++){
-        if(money[i].budget==amount.budget&&
-            money[i].expensename==amount.expensename&&
-            money[i].expenseprice==amount.expenseprice&&
-            money[i].totalbudget==amount.totalbudget) {
-                moneyIndex = i;
-            }
+    for (let i = 0; i < money.length; i++) {
+        if (money[i].budget == amount.budget &&
+            money[i].expensename == amount.expensename &&
+            money[i].expenseprice == amount.expenseprice &&
+            money[i].totalbudget == amount.totalbudget) {
+            moneyIndex = i;
+        }
     }
 
-    money.splice(moneyIndex,1);
+    money.splice(moneyIndex, 1);
     localStorage.setItem('Money', JSON.stringify(money));
 }
 
